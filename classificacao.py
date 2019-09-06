c6fc79c6e16ab1bd6825d037a15cdb8ee7d1f8bc
KaioDelmondes <kaioregodearaujo@hotmail.com>
2019-09-06 15:54:23 -0300

    Classificação: Interface do Scikit e classificaçao.py adequados à implementação de MLP do Scikit-learn

---
+++ classificacao.py
@@ -0,0 +1,21 @@
+from interfaces.scikitlearn_interface import MLP_sklearn
+import numpy
+from utils import is_matriz
+
+class MLP:
+	"""docstring for MLP"""
+	def __init__(self, camadas_ocultas=()):
+		self.modelo = MLP_sklearn(camadas_ocultas)
+
+	def treinar(self, entradas, attbr_classe):
+		self.modelo.treinar(entradas, attbr_classe)
+	
+	def estimar(self, attbr):
+		attbr = numpy.asarray(attbr)
+		if is_matriz(attbr):
+			return self.modelo.estimar_n(attbr)
+		else:
+			return self.modelo.estimar_um(attbr)
+
+	def precisao_do_modelo(self, entradas, attbr_classe):
+		return self.modelo.precisao(entradas, attbr_classe)
--- interfaces/scikitlearn_interface.py
+++ interfaces/scikitlearn_interface.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans
+from sklearn.neural_network import MLPClassifier
 
 def KMeans_sklearn(db,k):
 	try:
@@ -6,3 +7,20 @@ def KMeans_sklearn(db,k):
 		return kmeans_obj
 	except Exception as e:
 		raise e
+
+class MLP_sklearn:
+	def __init__(self, camadas_ocultas):
+		self.estimator = MLPClassifier(hidden_layer_sizes=camadas_ocultas)
+
+	def treinar(self, entradas, attbr_classe):
+		self.estimator.fit(entradas, attbr_classe.astype(int))
+	
+	def estimar_um(self, carac_entrada):
+		return self.estimator.predict(carac_entrada.reshape(1,-1))
+
+	def estimar_n(self, carac_entrada):
+		return self.estimator.predict(carac_entrada)
+	
+	def precisao(self, carac_entrada, saida_real):
+		prec_perc = self.estimator.score(carac_entrada, saida_real.astype(int))*100
+		return prec_perc
