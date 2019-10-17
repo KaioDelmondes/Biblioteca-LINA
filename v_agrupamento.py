b7081076458bc9b6aa98c6dc35f2e7e8a9e46426
KaioDelmondes <kaioregodearaujo@hotmail.com>
2019-10-16 22:05:01 -0300

    apagados arquivos da versão antiga

--- v_agrupamento.py
+++
@@ -1,9 +0,0 @@
-from sklearn.cluster import KMeans
-
-def agrupar_Kmeans(db,k):
-	return KMeans(n_clusters=k).fit(db)
-
-def get_groups(db,group_labels):
-	grupos = []
-	grupos = [db.groupby(group_labels.labels_).get_group(x) for x in set(group_labels.labels_)]
-	return grupos
